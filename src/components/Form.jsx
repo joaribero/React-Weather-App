cf4e8dc9e7c3e75969452926885de17e4904a9c8
Joaribero <joaribero_la12@hotmail.com>
2021-05-17 20:42:18 -0300

    Display the temperature

---
+++ src/components/Form.jsx
@@ -0,0 +1,71 @@
+import React,{useState} from 'react';
+import Error from '../components/Error.jsx'
+
+const Form = ({search,setSearch,setRequest}) => {
+    
+    //state
+    const [error,setError] = useState(false);
+    
+    // extract city and country
+    const {city,country} = search;
+
+    //func for insert elements into the state
+    const handleChange = e => {
+        //update the state
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    //when user submits the form
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        //validate
+        if (city.trim() === '' || country.trim() === '') {
+            setError(true);
+            return;
+        }
+
+        setError(false);
+        
+        //transfer to the main component
+        setRequest(true);
+    }
+    
+    return ( 
+        <form onSubmit={handleSubmit}>
+            
+            {error? <Error message="All fields are required"/> : null}
+
+            <div className="input-field col s12">
+                <input type="text" name="city" id="city" value={city}  onChange={handleChange}/>
+                <label htmlFor="city">City: </label>
+            </div>
+
+            <div className="input-field col s12">
+                <select name="country" id="country" value={country} onChange={handleChange}>
+                    <option value="">-- Select a country --</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
+                </select>
+                <label htmlFor="country">Country: </label>
+            </div>
+
+            <div className="input-field col s12">
+                <button 
+                    type="submit" 
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                    >Search weather</button>
+            </div>
+        </form>
+     );
+}
+ 
+export default Form;
